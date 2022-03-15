ed6a5f38c85da849d5a02427431516279204e24c
nviana <nvianaperez@gmail.com>
2022-03-15 17:17:27 +0100

    Tria Aventura with Node

--- tria.js
+++ tria.js
@@ -1,7 +1,7 @@
 const readlineSync = require("readline-sync"); //aquesta llibreria "readline-sync" permet preguntar coses a l'usuari
 
 historia = [
-  { index: 0, text: "Funciona", s: 5, n: 1, fi: false },
+  { index: 0, text: "Funciona?", s: 5, n: 1, fi: false },
   { index: 1, text: "¿Lo has tocado", s: 6, n: 2, fi: false },
   { index: 2, text: "¿Nos dará problemas?", s: 3, n: 9, fi: false },
   { index: 3, text: "La has cagado", s: 4, n: 3, fi: false },
@@ -11,42 +11,33 @@ historia = [
   { index: 7, text: "¿Alguien lo sabe?", s: 3, n: 8, fi: false },
   { index: 8, text: "¡Escóndelo!", s: 10, n: 10, fi: false },
   { index: 9, text: "¡Lánzalo!", s: 10, n: 10, fi: false },
-  { index: 10, text: "Sin problemas", s: 10, n: 10, fi: true },
+  { index: 10, text: "Sin problema", s: 10, n: 10, fi: true },
 ];
 
 let nom = readlineSync.question("Com et dius? ");
-console.log("Hola " + nom + ", comencem!");
+console.log(
+  "Hola " +
+    nom +
+    ", aquí comença la teva història, només tu decideixes el teu camí..."
+);
 
 let index = 0;
+
 while (index >= 0) {
   let pagina = historia[index];
   console.log(pagina.text);
-  if (pagina.fi) {
-    index = -1;
-  } else {
-    if (pregunta()) {
-      index = pagina.si;
-    } else index = pagina.no;
-  }
-}
-
-function pregunta() {
   let resposta = "";
-  resposta = readlineSync.question(`Si o no? [s/n]`);
-  while (!respostaValida(resposta)) {
-    resposta = readlineSync.question(`Si o no? [s/n]`);
-  }
-  if (resposta === "s") {
-    return true;
-  } else {
-    return false;
-  }
-}
+  resposta = readlineSync.question(`Si o no? [s/n]`).toLowerCase();
 
-function respostaValida(r) {
-  if (r.toLowerCase() === "s" || r.toLowerCase(r) === "n") {
-    return true;
+  if (resposta === "s") {
+    index = pagina.s;
+    if (pagina.fi) {
+      index = -1;
+    }
   } else {
-    return false;
+    index = pagina.n;
+    if (pagina.fi) {
+      index = -1;
+    }
   }
 }
